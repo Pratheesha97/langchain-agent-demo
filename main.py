f07e039f11276ea36e423aa25bace5f436e3ae1f
Pratheesha97 <samadhipratheesha@gmail.com>
2025-11-24 10:58:05 +0530

    Replace OpenAI with Gemini

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from langchain.tools import tool
-from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
-from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv, find_dotenv
 import re
+import getpass
+import os
 
-load_dotenv()
+
+load_dotenv(find_dotenv(), override=True)
+
+if 'GOOGLE_API_KEY' not in os.environ:
+    os.environ['GOOGLE_API_KEY'] = getpass.getpass('Provide your Google API Key: ')
 
 @tool
 def add_numbers(query: str) -> str:
@@ -12,8 +18,13 @@ def add_numbers(query: str) -> str:
     nums = re.findall(r"\d+", query)
     return str(int(nums[0]) + int(nums[1]))
 
-llm = ChatOpenAI(model="gpt-4o")
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash', temperature=0.9)
 
 agent = create_agent(llm, [add_numbers])
 
-print(agent.invoke({"input": "What is 12 + 5?"}))
+result = agent.invoke({
+    "messages": [{"role": "user", "content": "What is 12 - 5?"}]
+})
+
+print(result["messages"][-1].content)
+
